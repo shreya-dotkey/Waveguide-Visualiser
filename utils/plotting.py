7660af6a8a659543083622d287466f196899cd45
shreya-dotkey <shreyaham@gmail.com>
2025-11-17 20:20:51 +0530

    Update plotting.py

--- utils/plotting.py
+++ utils/plotting.py
@@ -1,1 +1,16 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+def create_streamplot(x, y, U, V, title=None):
+    """
+    Create a simple Matplotlib streamplot and return the figure.
+    """
+    fig, ax = plt.subplots(figsize=(5, 5))
+    ax.streamplot(x, y, U, V)
+    ax.set_aspect('equal')
+
+    if title:
+        ax.set_title(title)
+
+    return fig
 
